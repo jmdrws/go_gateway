cb7acc65e04e54addd9c2ffbc5937ea0c32fa146
17859658698 <835888055@qq.com>
2022-07-04 11:10:45 +0800

    密码修改功能完成；管理员功能接口基本实现完成

--- controller/admin.go
+++ controller/admin.go
@@ -3,8 +3,10 @@ package controller
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/e421083458/golang_common/lib"
 	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
+	"github.com/jmdrws/go_gateway/dao"
 	"github.com/jmdrws/go_gateway/dto"
 	"github.com/jmdrws/go_gateway/middleware"
 	"github.com/jmdrws/go_gateway/public"
@@ -15,9 +17,10 @@ type AdminController struct{}
 func AdminRegister(group *gin.RouterGroup) {
 	adminLogin := &AdminController{}
 	group.GET("/admin_info", adminLogin.AdminInfo)
+	group.POST("/change_pwd", adminLogin.ChangePwd)
 }
 
-// AdminLogin godoc
+// AdminInfo godoc
 // @Summary 管理员信息
 // @Description 管理员信息
 // @Tags 管理员接口
@@ -45,4 +48,57 @@ func (adminLogin *AdminController) AdminInfo(c *gin.Context) {
 		Roles:        []string{"admin"},
 	}
 	middleware.ResponseSuccess(c, out)
+}
+
+// ChangePwd godoc
+// @Summary 修改密码
+// @Description 修改密码
+// @Tags 管理员接口
+// @ID /admin/change_pwd
+// @Accept  json
+// @Produce  json
+// @Param body body dto.ChangePwdInput true "body"
+// @Success 200 {object} middleware.Response{data=string} "success"
+// @Router /admin/change_pwd [post]
+func (adminLogin *AdminController) ChangePwd(c *gin.Context) {
+	params := &dto.ChangePwdInput{}
+	if err := params.BindValidParam(c); err != nil {
+		middleware.ResponseError(c, 200, err)
+		return
+	}
+
+	//1. session读取用户信息到结构体 sessInfo
+	//2. sessInfo.ID 读取数据库信息 adminInfo
+	//3. params.password+adminInfo.salt sha256 saltPassword
+	//4. saltPassword==> adminInfo.password 执行数据保存
+
+	//从session获取
+	sess := sessions.Default(c)
+	sessInfo := sess.Get(public.AdminSessionInfoKey)
+	adminSessionInfo := &dto.AdminSessionInfo{}
+	if err := json.Unmarshal([]byte(fmt.Sprint(sessInfo)), adminSessionInfo); err != nil {
+		middleware.ResponseError(c, 2000, err)
+		return
+	}
+	//从数据库中读取adminInfo
+	tx, err := lib.GetGormPool("default")
+	if err != nil {
+		middleware.ResponseError(c, 2001, err)
+		return
+	}
+	adminInfo := &dao.Admin{}
+	adminInfo, err = adminInfo.Find(c, tx, &dao.Admin{UserName: adminSessionInfo.UserName})
+	if err != nil {
+		middleware.ResponseError(c, 2002, err)
+		return
+	}
+	//生成新密码saltPassword
+	saltPassword := public.GenSaltPassword(adminInfo.Salt, params.Password)
+	adminInfo.Password = saltPassword
+	//执行数据保存
+	if err := adminInfo.Save(c, tx); err != nil {
+		middleware.ResponseError(c, 2003, err)
+		return
+	}
+	middleware.ResponseSuccess(c, "")
 }
