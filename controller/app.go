9a82210ac4a4899418672c0776aa99b1e7fb28fe
17859658698 <835888055@qq.com>
2022-07-20 20:52:08 +0800

    租户模块初始化和租户列表查询功能初始化

---
+++ controller/app.go
@@ -0,0 +1,65 @@
+package controller
+
+import (
+	"github.com/e421083458/golang_common/lib"
+	"github.com/gin-gonic/gin"
+	"github.com/jmdrws/go_gateway/dao"
+	"github.com/jmdrws/go_gateway/dto"
+	"github.com/jmdrws/go_gateway/middleware"
+)
+
+func APPRegister(router *gin.RouterGroup) {
+	app := APPController{}
+	router.GET("/app_list", app.APPList)
+}
+
+type APPController struct {
+}
+
+// APPList godoc
+// @Summary 租户列表
+// @Description 租户列表
+// @Tags 租户管理
+// @ID /app/app_list
+// @Accept  json
+// @Produce  json
+// @Param info query string false "关键词"
+// @Param page_size query string true "每页多少条"
+// @Param page_no query string true "页码"
+// @Success 200 {object} middleware.Response{data=dto.APPListOutput} "success"
+// @Router /app/app_list [get]
+func (admin *APPController) APPList(c *gin.Context) {
+	params := &dto.APPListInput{}
+	if err := params.GetValidParams(c); err != nil {
+		middleware.ResponseError(c, 2000, err)
+		return
+	}
+	info := &dao.App{}
+	list, total, err := info.APPList(c, lib.GORMDefaultPool, params)
+	if err != nil {
+		middleware.ResponseError(c, 2001, err)
+		return
+	}
+	var outputList []dto.APPListItemOutput
+	for _, item := range list {
+		realQps := int64(0)
+		realQpd := int64(0)
+		outputList = append(outputList, dto.APPListItemOutput{
+			ID:       item.ID,
+			AppID:    item.AppID,
+			Name:     item.Name,
+			Secret:   item.Secret,
+			WhiteIPS: item.WhiteIPS,
+			Qpd:      item.Qpd,
+			Qps:      item.Qps,
+			RealQpd:  realQpd,
+			RealQps:  realQps,
+		})
+	}
+	output := dto.APPListOutput{
+		List:  outputList,
+		Total: total,
+	}
+	middleware.ResponseSuccess(c, output)
+	return
+}
---
+++ dto/app.go
@@ -0,0 +1,37 @@
+package dto
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jmdrws/go_gateway/public"
+	"time"
+)
+
+type APPListInput struct {
+	Info     string `json:"info" form:"info" comment:"查找信息" validate:""`
+	PageSize int    `json:"page_size" form:"page_size" comment:"页数" validate:"required,min=1,max=999"`
+	PageNo   int    `json:"page_no" form:"page_no" comment:"页码" validate:"required,min=1,max=999"`
+}
+
+func (params *APPListInput) GetValidParams(c *gin.Context) error {
+	return public.DefaultGetValidParams(c, params)
+}
+
+type APPListItemOutput struct {
+	ID        int64     `json:"id" gorm:"primary_key"`
+	AppID     string    `json:"app_id" gorm:"column:app_id" description:"租户id	"`
+	Name      string    `json:"name" gorm:"column:name" description:"租户名称	"`
+	Secret    string    `json:"secret" gorm:"column:secret" description:"密钥"`
+	WhiteIPS  string    `json:"white_ips" gorm:"column:white_ips" description:"ip白名单，支持前缀匹配		"`
+	Qpd       int64     `json:"qpd" gorm:"column:qpd" description:"日请求量限制"`
+	Qps       int64     `json:"qps" gorm:"column:qps" description:"每秒请求量限制"`
+	RealQpd   int64     `json:"real_qpd" description:"日请求量限制"`
+	RealQps   int64     `json:"real_qps" description:"每秒请求量限制"`
+	UpdatedAt time.Time `json:"create_at" gorm:"column:create_at" description:"添加时间	"`
+	CreatedAt time.Time `json:"update_at" gorm:"column:update_at" description:"更新时间"`
+	IsDelete  int8      `json:"is_delete" gorm:"column:is_delete" description:"是否已删除；0：否；1：是"`
+}
+
+type APPListOutput struct {
+	List  []APPListItemOutput `json:"list" form:"list" comment:"租户列表"`
+	Total int64               `json:"total" form:"total" comment:"租户总数"`
+}
