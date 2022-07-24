9e0a57590b472785b4307b730db2c948d4ddeed2
17859658698 <835888055@qq.com>
2022-07-24 16:11:55 +0800

    main.go中开启多个终端，用于实现代码混用 -my_config=./conf/dev/ -endpoint=dashboard或 -my_config=./conf/dev/ -endpoint=server

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/e421083458/golang_common/lib"
 	"github.com/jmdrws/go_gateway/router"
 	"os"
@@ -8,14 +10,43 @@ import (
 	"syscall"
 )
 
+//endpoint dashboard后台管理  server代理服务器
+//config ./conf/prod/ 对应配置文件夹
+var (
+	endpoint = flag.String("endpoint", "", "input endpoint dashboard or server")
+	config   = flag.String("my_config", "", "input config file like ./conf/dev/")
+)
+
 func main() {
-	lib.InitModule("./conf/dev/", []string{"base", "mysql", "redis"})
-	defer lib.Destroy()
-	router.HttpServerRun()
+	flag.Parse()
+	if *endpoint == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+	if *config == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
 
-	quit := make(chan os.Signal)
-	signal.Notify(quit, syscall.SIGKILL, syscall.SIGQUIT, syscall.SIGINT, syscall.SIGTERM)
-	<-quit
+	if *endpoint == "dashboard" {
+		lib.InitModule(*config, []string{"base", "mysql", "redis"})
+		defer lib.Destroy()
+		router.HttpServerRun()
 
-	router.HttpServerStop()
+		quit := make(chan os.Signal)
+		signal.Notify(quit, syscall.SIGKILL, syscall.SIGQUIT, syscall.SIGINT, syscall.SIGTERM)
+		<-quit
+
+		router.HttpServerStop()
+	} else {
+		lib.InitModule(*config, []string{"base", "mysql", "redis"})
+		defer lib.Destroy()
+
+		fmt.Println("start service")
+		//todo
+
+		quit := make(chan os.Signal)
+		signal.Notify(quit, syscall.SIGKILL, syscall.SIGQUIT, syscall.SIGINT, syscall.SIGTERM)
+		<-quit
+	}
 }
